8b2c92ae541c0b4eb5558cb0d75d29445062c046
nathan <ngrasset@student.42.fr>
2017-04-08 18:56:19 +0200

    game object manager

--- includes/Shape.hpp
+++ includes/Shape.hpp
@@ -1,34 +1,37 @@
 #ifndef SHAPE_HPP
-# define SHAPE_HPP
+#define SHAPE_HPP
 
 #include <iostream>
+#include <Vector2.class.hpp>
 
-class Shape
-{
-
-public:
-
+class Shape {
+   public:
+	Shape();
 	Shape(int width, int height, int posX, int posY, std::string shape);
-	Shape(Shape const &src);
+	Shape(Shape const& src);
 	~Shape(void);
 
-	int getWidth(void);
-	int getHeight(void);
-	int getPosX(void);
-	int getPosY(void);
-	std::string getshape(void);
+	int getWidth(void) const;
+	int getHeight(void) const;
 
-	Shape &operator=(Shape const &rhs);
+	int getPosX(void) const;
+	int getPosY(void) const;
+	Vector2 getPosition() const;
+	std::string getshape(void) const;
 
-private:
+	Shape& operator=(Shape const& rhs);
+
+	void setPosition(int x, int y);
+	void setPosition(Vector2 v);
+
+	void setShape(std::string s);
+
+   private:
 	int _width;
 	int _height;
 	int _posX;
 	int _posY;
 	std::string _shape;
-
-	Shape(void);
-
 };
 
 #endif
--- src/Shape.cpp
+++ src/Shape.cpp
@@ -1,33 +1,41 @@
 #include "../includes/Shape.hpp"
 
 
-Shape::Shape(int width, int height, int posX, int posY, std::string shape) : _width(width), _height(height), _posX(posX), _posY(posY), _shape(shape)
-{
-	//std::cout<<"Default constructor called"<<std::endl;
+Shape::Shape() : _width(0), _height(0), _posX(0), _posY(0), _shape("") {}
+
+Shape::Shape(int width, int height, int posX, int posY, std::string shape)
+    : _width(width), _height(height), _posX(posX), _posY(posY), _shape(shape) {
+	// std::cout<<"Default constructor called"<<std::endl;
 }
 
-Shape::Shape(Shape const &src)
-{
-	//std::cout<<"Copy constructor called"<<std::endl;
+Shape::Shape(Shape const& src) {
+	// std::cout<<"Copy constructor called"<<std::endl;
 	*this = src;
 }
 
-Shape::~Shape(void)
-{
-	//std::cout<<"Destructor called"<<std::endl;
+Shape::~Shape(void) {
+	// std::cout<<"Destructor called"<<std::endl;
 }
 
-int Shape::getWidth(void) { return _width; }
-int Shape::getHeight(void) { return _height; }
-int Shape::getPosX(void) { return _posX; }
-int Shape::getPosY(void) { return _posY; }
-std::string Shape::getshape(void) { return _shape; }
+int Shape::getWidth(void) const {
+	return _width;
+}
+int Shape::getHeight(void) const {
+	return _height;
+}
+int Shape::getPosX(void) const {
+	return _posX;
+}
+int Shape::getPosY(void) const {
+	return _posY;
+}
+std::string Shape::getshape(void) const {
+	return _shape;
+}
 
-Shape &Shape::operator=(Shape const &rhs)
-{
-	//std::cout<<"Assignation operator called"<<std::endl;
-	if (&rhs != this)
-	{
+Shape& Shape::operator=(Shape const& rhs) {
+	// std::cout<<"Assignation operator called"<<std::endl;
+	if (&rhs != this) {
 		_width = rhs._width;
 		_height = rhs._height;
 		_posX = rhs._posX;
@@ -38,3 +46,20 @@ Shape &Shape::operator=(Shape const &rhs)
 	return *this;
 }
 
+void Shape::setPosition(int x, int y) {
+	_posX = x;
+	_posY = y;
+}
+
+void Shape::setPosition(Vector2 v) {
+	_posX = v.x;
+	_posY = v.y;
+}
+
+Vector2 Shape::getPosition() const {
+	return Vector2(_posX, _posY);
+}
+
+void Shape::setShape(std::string s) {
+	_shape = s;
+}
