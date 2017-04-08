a27a10ea7b9d7b2a3465d5e139931edda0daf2b4
Amine DERRAGUI <aderragu@e2r8p15.42.fr>
2017-04-08 16:10:39 +0200

    ajout NDisplay + petit main

---
+++ includes/NDisplay.hpp
@@ -0,0 +1,50 @@
+#ifndef NDISPLAY_HPP
+# define NDISPLAY_HPP
+
+#include <iostream>
+#include <ncurses.h>
+
+#define NB_W_MAX 10
+
+class NDisplay
+{
+
+public:
+	static int sNbWindows;
+
+	NDisplay(void);
+	NDisplay(NDisplay const &src);
+	~NDisplay(void);
+	
+	// Display function
+	void print(char c);
+	void print(char c, int x, int y);
+	void print(int id, char c, int x, int y);
+
+	void prints(std::string s);
+	void prints(std::string s, int x, int y);
+	void prints(int id, std::string s, int x, int y);
+	// End Display function
+	
+	void rfh(void);
+	void clr(void);
+	void clrW(int id);
+
+	void waitForQuit(char c);
+	void waitForQuitW(int id, char c);
+	
+	void subWinStdr(int id, int width, int height, int x, int y);
+
+	int getX(void)const;
+	int getY(void)const;
+
+
+	NDisplay &operator=(NDisplay const &rhs);
+
+private:
+	
+	WINDOW *_Windows[NB_W_MAX];
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
-int main(void) {
-	return 0;
+#include <NDisplay.hpp>
+#include <ncurses.h>
+int main ()
+{	
+	NDisplay mainWindow;
+
+	mainWindow.prints("salut", mainWindow.getX()/2, mainWindow.getY()/2);
+	while (1)
+	{
+		mainWindow.waitForQuit(27);
+	}
 }
