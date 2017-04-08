f7514d72509aad2e7e45415c6f3622b4517e5fba
nathan <ngrasset@student.42.fr>
2017-04-08 21:17:28 +0200

    stuff

--- src/Game.class.cpp
+++ src/Game.class.cpp
@@ -3,9 +3,7 @@
 #include <stdlib.h>
 
 
-Game::Game(void) {
-
-}
+Game::Game(void) {}
 
 Game::Game(const Game& src) {
 	*this = src;
@@ -21,7 +19,8 @@ Game& Game::operator=(const Game& rhs) {
 
 void Game::start() {
 	_state = PLAYING;
-	//_mainWindow.prints("salut", _mainWindow.getX() / 2, _mainWindow.getY() / 2);
+	//_mainWindow.prints("salut", _mainWindow.getX() / 2, _mainWindow.getY() /
+	// 2);
 	gameLoop();
 	_state = EXITING;
 }
@@ -29,7 +28,8 @@ void Game::start() {
 void Game::gameLoop() {
 	while (_state == PLAYING) {
 		_inputManager.readInput();
-		_mainWindow.draw(*(new Shape(1,1, Game::_mainWindow.getX()/2, _mainWindow.getY()/2,"^")));
+		_mainWindow.draw(*(new Shape(1, 1, Game::_mainWindow.getX() / 2,
+		                             _mainWindow.getY() / 2, "^")));
 		// update
 		// draw
 		usleep(100);
@@ -48,6 +48,11 @@ NDisplay const& Game::getWindow() {
 	return _mainWindow;
 }
 
+GameObjectManager const& Game::getGameObjectManager() {
+	return _gameObjectManager;
+}
+
 InputManager Game::_inputManager;
 NDisplay Game::_mainWindow;
 Game::eGameState Game::_state = UNINITIALIZED;
+GameObjectManager Game::_gameObjectManager;
