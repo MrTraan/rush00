8f7b7333619c33c05450226edb62d5b768c2d9be
Amine DERRAGUI <aderragu@e2r8p15.42.fr>
2017-04-09 06:42:53 +0200

    ajout class zorg

---
+++ includes/Zorg.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Shape.hpp"
+#include "GameObject.class.hpp"
+#include "Game.class.hpp"
+
+class Zorg : public GameObject {
+
+public:
+
+	public:
+	Zorg(void);
+	Zorg(Shape s);
+	Zorg(const Zorg& src);
+	~Zorg(void);
+
+	Zorg& operator=(const Zorg& rhs);
+
+	void collideWithPlayerLaser();
+
+	virtual void update();
+
+   protected:
+   	Vector2 _behaviour[11];
+	int _ind;
+
+
+
+};
---
+++ src/Zorg.cpp
@@ -0,0 +1,91 @@
+#include "../includes/Zorg.hpp"
+
+Zorg::Zorg(void) {}
+
+Zorg::Zorg(Shape s) : GameObject(s) {
+
+	_behaviour[0].x = 1;
+	_behaviour[0].y = 0;
+
+	_behaviour[1].x = 1;
+	_behaviour[1].y = 0;
+
+	_behaviour[2].x = 1;
+	_behaviour[2].y = 0;
+
+	_behaviour[3].x = 1;
+	_behaviour[3].y = 0;
+
+	_behaviour[4].x = 1;
+	_behaviour[4].y = 0;
+	
+	_behaviour[5].x = 1;
+	_behaviour[5].y = 0;
+
+	_behaviour[6].x = -1;
+	_behaviour[6].y = 1;
+
+	_behaviour[7].x = -1;
+	_behaviour[7].y = 1;
+
+	_behaviour[8].x = -1;
+	_behaviour[8].y = 1;
+
+	_behaviour[9].x = -1;
+	_behaviour[9].y = 1;
+
+	_behaviour[10].x = -1;
+	_behaviour[10].y = 1;
+
+	_behaviour[10].x = -1;
+	_behaviour[10].y = 1;
+	}
+
+Zorg::Zorg(Zorg const &src)
+{
+	//std::cout<<"Copy constructor called"<<std::endl;
+	*this = src;
+}
+
+Zorg::~Zorg(void)
+{
+	//std::cout<<"Destructor called"<<std::endl;
+}
+
+void Zorg::collideWithPlayerLaser() {
+	alive = false;
+}
+
+void Zorg::update() {
+	Vector2 pos = _shape.getPosition();
+
+	if (Game::tick % 2)
+	{
+		if (_ind >= 11)
+			_ind = 0;
+		pos.x += _behaviour[_ind].x;
+		pos.y += _behaviour[_ind].y;
+		_ind++;
+	}
+	
+
+	_shape.setPosition(pos);
+
+
+	for (Node* itr = Game::getGameObjectManager()._gameObjectList.begin(); itr;
+	     itr = itr->next) {
+		if (itr->go != this) {  // prevent self collision
+			if (collide(*(itr->go))) {
+				itr->go->collideWithEnnemy();
+			}
+		}
+	}
+}
+Zorg &Zorg::operator=(Zorg const &rhs)
+{
+	//std::cout<<"Assignation operator called"<<std::endl;
+	if (&rhs != this)
+		_ind = rhs._ind;
+	return *this;
+}
+
